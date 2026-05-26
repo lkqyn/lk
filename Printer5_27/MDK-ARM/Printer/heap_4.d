c209edcd7ce9e0130a46eefa49c3e903879cdb28
lk <2405104813@qq.com>
2026-05-26 13:36:09 +0800

    参加展会最终版

---
+++ Printer5_27/MDK-ARM/Printer/heap_4.d
@@ -0,0 +1,13 @@
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+printer\heap_4.o: E:\Keil5\ARM\ARMCC\Bin\..\include\stdlib.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+printer\heap_4.o: E:\Keil5\ARM\ARMCC\Bin\..\include\stddef.h
+printer\heap_4.o: E:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
+printer\heap_4.o: ../Core/Inc/FreeRTOSConfig.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+printer\heap_4.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
